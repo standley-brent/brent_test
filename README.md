228baa8402d9327abdb5db2729003ce2a4ba5fab
standley-brent <52802637+standley-brent@users.noreply.github.com>
2025-12-15 20:28:13 -0700

    logging and prometheus

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # brent_test
+### Sources
+### Methodology
+### Preferred Solution
+- According to the spec the solution needs a single endpoint that can determine whether the request is for a single number (query) or a range of numbers (min & max)
+- My solution to this is a single function with a big if..else statement to determine which parameters have been used.
+- I think it would be better to separate these endpoints into different units, making development and testing easier.
+- Example: an endpoint like `romannumeral/query` and a second one called `romannumeral/range`
+- Then my project structure could have separate folders per endpoint and separate test files per endpoint.
--- converter.py
+++ converter.py
@@ -1,7 +1,13 @@
+import logging
 from concurrent.futures import ProcessPoolExecutor
 from flask import Flask, jsonify, request
 from utils.convert_to_roman import convert_to_roman
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+print("Registered routes:", [str(rule) for rule in app.url_map.iter_rules()])
+logging.basicConfig(level=logging.INFO)
 
 # Flask route to convert numeral
 @app.route('/romannumeral/', methods=['GET'])
@@ -9,6 +15,7 @@ app = Flask(__name__)
 def numeral_converter():
     lower_bound = 1
     upper_bound = 3999
+    logging.info(f"Received request: {request.args}")
 
     # 1. 'query' paramater given, single conversion wanted
     query = request.args.get('query')
@@ -16,12 +23,15 @@ def numeral_converter():
         try:
             number_input = int(query)
         except ValueError:
+            logging.error(f"Error: expected an integer, received {query}")
             return jsonify({'error': 'Expected an integer'}), 400
 
         if  lower_bound <= number_input <= upper_bound:
             return jsonify({'input': query, 'output': convert_to_roman(number_input)})
         else:
+            logging.error(f"Input number {number_input} is out of range.")
             return jsonify({'error': f"Input number {number_input} is out of range. Please enter a number between {lower_bound} and {upper_bound}."}), 400
+        
         
     # 2. 'min' & 'max' parameters given: range wanted
     min_val = request.args.get('min')
@@ -32,9 +42,11 @@ def numeral_converter():
             min_val = int(min_val)
             max_val = int(max_val)
         except ValueError:
-            return jsonify({'error': 'Min and max must be integers'}), 400
+            logging.error(f"Error: Min and Max must be integers: {query}")
+            return jsonify({'Error': 'Min and Max must be integers'}), 400
 
         if min_val < lower_bound or max_val > upper_bound or min_val > max_val:
+            logging.error(f"Input number {number_input} is out of range.")
             return jsonify({'error': f'Invalid range. Ensure {lower_bound} <= min <= max <= {upper_bound}'}), 400
 
         # Create list of numbers to convert
@@ -51,15 +63,13 @@ def numeral_converter():
         ]
 
         return jsonify({'conversions': results})
+    logging.error(f"Error: Missing required parameters. Provide either 'query' or both 'min' and 'max'.")
     return jsonify({'error': 'Missing required parameters. Provide either "query" or both "min" and "max".'}), 400
-    
-if __name__ == '__main__':
-    app.run(debug=True)
 
-# TODO: I'm not sure i'm even doing the right 400 responses, etc.
-# TODO: add preferred method with different functions and urls per type of request
+if __name__ == '__main__':
+    app.run(debug=True,port=5001,use_reloader=False)
+
 # TODO: add comments everywhere
-# TODO: add logging
 # TODO: add rate limiting
 # TODO: add password?
 # TODO: dockerize
